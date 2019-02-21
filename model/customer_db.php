5c2ad9f558e39350ce02cfd3d9e5530775643fa7
Matthew0007 <taiwo.obadare@gmail.com>
2019-02-21 21:13:52 +0000

    To try and change the copied code to suit customer_db file, for example get_customer() function and get_customer_name($customer_id) function and the add_customers($name) function

--- model/customer_db.php
+++ model/customer_db.php
@@ -5,62 +5,38 @@ function get_customers() {
               ORDER BY customer_id';
     $statement = $db->prepare($query);
     $statement->execute();
-    $customers = $statement->fetchAll();
-    $statement->closeCursor();
-    return $customers;
+    return $statement; 
 }
 
-function get_product($product_id) {
+function get_customer_name($customer_id) {
     global $db;
-    $query = 'SELECT * FROM products
-              WHERE productID = :product_id';
+    $query = 'SELECT * FROM categories
+              WHERE customer_id = :customerID';    
     $statement = $db->prepare($query);
-    $statement->bindValue(":product_id", $product_id);
-    $statement->execute();
-    $product = $statement->fetch();
-    $statement->closeCursor();
-    return $product;
+    $statement->bindValue(':customerID', $category_id);
+    $statement->execute();    
+    $customer = $statement->fetch();
+    $statement->closeCursor();    
+    $customer_name = $customer['customer_name'];
+    return $customer_name;
 }
 
-function delete_product($product_id) {
+function add_customers($name) {
     global $db;
-    $query = 'DELETE FROM products
-              WHERE productID = :product_id';
+    $query = 'INSERT INTO customers (customer_name)
+              VALUES (:name)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':product_id', $product_id);
-    $statement->execute();
-    $statement->closeCursor();
-}
-
-function add_product($category_id, $code, $name, $price) {
-    global $db;
-    $query = 'INSERT INTO products
-                 (categoryID, productCode, productName, listPrice)
-              VALUES
-                 (:category_id, :code, :name, :price)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
-    $statement->bindValue(':code', $code);
     $statement->bindValue(':name', $name);
-    $statement->bindValue(':price', $price);
     $statement->execute();
-    $statement->closeCursor();
+    $statement->closeCursor();    
 }
 
-function update_product($product_id, $category_id, $code, $name, $price) {
+function delete_customer($customer_id) {
     global $db;
-    $query = 'UPDATE products
-              SET categoryID = :category_id,
-                  productCode = :code,
-                  productName = :name,
-                  listPrice = :price
-               WHERE productID = :product_id';
+    $query = 'DELETE FROM customers
+              WHERE customer_id = :customerID';
     $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
-    $statement->bindValue(':code', $code);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':price', $price);
-    $statement->bindValue(':product_id', $product_id);
+    $statement->bindValue(':customerID', $customer_id);
     $statement->execute();
     $statement->closeCursor();
 }
