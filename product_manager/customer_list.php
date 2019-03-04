dd1e0b5c63cad79c03325cdcda365924848d8231
Matthew0007 <taiwo.obadare@gmail.com>
2019-03-04 15:25:45 +0000

    Copies category_list.php and amended code to customer

--- product_manager/customer_list.php
+++ product_manager/customer_list.php
@@ -1,24 +1,20 @@
 <?php include '../view/header.php'; ?>
 <main>
 
-    <h1>Customer List</h1>
+    <h1>Caustomer List</h1>
     <table>
         <tr>
             <th>Name</th>
-       
-            <th>Customer ID</th>
             <th>&nbsp;</th>
         </tr>
-        <?php foreach ($customers as $customer):?>
+        <?php foreach ($customers as $customer) : ?>
         <tr>
             <td><?php echo $customer['customer_name']; ?></td>
-            <td><?php echo $customer['customer_id']; ?></td>
             <td>
-                <form id="delete_product_form"
-                      action="index.php" method="post">
+                <form id="delete_product_form" action="index.php" method="post">
                     <input type="hidden" name="action" value="delete_customer">
-                    <input type="hidden" name="customer_id"
-                           value="<?php echo $customer['customer_id']; ?>">
+                    <input type="hidden" name="customer_id" value="<?php echo $customer['customer_id']; ?>">
+                    
                     <input type="submit" value="Delete">
                 </form>
             </td>
@@ -27,23 +23,15 @@
     </table>
     <br />
 
-<!--    <h2>Add Customer</h2>
+    <h2>Add Customer</h2>
     <form id="add_customer_form"
           action="index.php" method="post">
         <input type="hidden" name="action" value="add_customer">
-        
-        <label>Customer:</label>
-        <select id="id">
-        <?php foreach ( $customers as $customer ) : ?>
-            <option value="<?php echo $customer['productID']; ?>">
-            </option>
-        <?php endforeach; ?>
-        </select>
 
         <label>Name:</label>
         <input type="input" name="name">
         <input type="submit" value="Add">
-    </form>-->
+    </form>
 
     <p><a href="index.php?action=list_products">List Products</a></p>
 
