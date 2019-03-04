fa17351095d739360052d9634f5ceae8d54017c6
Matthew0007 <taiwo.obadare@gmail.com>
2019-03-04 12:26:36 +0000

    Changed h1 tag from Menu to Database

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'view/header.php'; ?>
 <link href="css/main.css" rel="stylesheet" type="text/css"/>
 <main>
-    <h1>Menu</h1>
+    <h1>Database</h1>
     <ul>
         <li>
             <a href="product_manager">PRODUCT MANAGER</a>
