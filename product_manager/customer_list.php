2ecfadec0b42978f40ec08ecabfed53bafd30a4f
Matthew0007 <taiwo.obadare@gmail.com>
2019-03-04 15:46:28 +0000

    Commented out delete input type and added id of name input type, also changed customer to customers

--- product_manager/customer_list.php
+++ product_manager/customer_list.php
@@ -12,10 +12,10 @@
             <td><?php echo $customer['customer_name']; ?></td>
             <td>
                 <form id="delete_product_form" action="index.php" method="post">
-                    <input type="hidden" name="action" value="delete_customer">
+                    <input type="hidden" name="action" value="delete_customers">
                     <input type="hidden" name="customer_id" value="<?php echo $customer['customer_id']; ?>">
                     
-                    <input type="submit" value="Delete">
+<!--                    <input type="submit" value="Delete">-->
                 </form>
             </td>
         </tr>
@@ -26,10 +26,12 @@
     <h2>Add Customer</h2>
     <form id="add_customer_form"
           action="index.php" method="post">
-        <input type="hidden" name="action" value="add_customer">
+        <input type="hidden" name="action" value="add_customers">
 
         <label>Name:</label>
         <input type="input" name="name">
+        <label>id:</label>
+        <input type="input" name="id">
         <input type="submit" value="Add">
     </form>
 
